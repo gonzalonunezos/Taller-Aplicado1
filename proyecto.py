19cc3684f6bf595ee4462a07069d9a966daeae00
gonzalonunezos <gonzalo.bbs@gmail.com>
2016-11-15 08:12:20 -0400

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -2,7 +2,7 @@ import random
 def listaAleatorios(n):
       lista = [0]  * n
       for i in range(6):
-          lista[i] = random.randint(1,36)
+          lista[i] = random.randint(1,41)
       return lista
 def listaordenada(lista):
       listaAleatorios.sort()
